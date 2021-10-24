18c85368762d322e85d5c1897608a2b0ca3d5794
architjen <architjen010@gmail.com>
2021-10-25 01:06:19 +0200

    testing get and put

--- test.py
+++ test.py
@@ -3,5 +3,7 @@ import requests
 BASE = 'http://127.0.0.1:5000/'
 
 response = requests.get(BASE + 'investments')
+print(response.json())
 
+response = requests.post(BASE + 'investments')
 print(response.json())
