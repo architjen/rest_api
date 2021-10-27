d2f549847f2039ef102bbf5f630540a09ded80d4
architjen <architjen010@gmail.com>
2021-10-27 19:48:52 +0200

    add post in app

--- app.py
+++ app.py
@@ -13,15 +13,16 @@ app.config["DEBUG"] = True
 # ref data: https://www.data.gouv.fr/fr/datasets/operations-de-construction-et-de-renovation-dans-les-lycees-francilens/#
 investments_path = './data/investments.csv'
 
-investment_put_args = reqparse.RequestParser()
+'''investment_put_args = reqparse.RequestParser()
 investment_put_args.add_argument("lycee", type=str, required=True)
 investment_put_args.add_argument("annee_de_livraison", type=int)
 investment_put_args.add_argument("ville", type=str, required=True)
-investment_put_args.add_argument("titreoperation", type=str)
-investment_put_args.add_argument("codeuai", type=str, required=True)
+investment_put_args.add_argument("codeuai", type=str, required=True)'''
 
-# TO DO: add these once the testing has been done on the initial batch of data
+# TO DO: add these once the testing has been validated on the initial batch of data
+
 '''investment_put_args.add_argument("agent", type=str, help="Name of the company", required=True)
+investment_put_args.add_argument("titreoperation", type=str)
 investment_put_args.add_argument("ppi", type=str, help="Delivery year")
 investment_put_args.add_argument("high school", type=str, help="Name of the operation is required")
 investment_put_args.add_argument("market_notification", type=str, help="Name of the company")
@@ -43,6 +44,34 @@ class Investments(Resource):
         data = data.to_dict() # getting only 5 top data
         return data, 200 
 
+    def post(self):
+
+        investment_put_args = reqparse.RequestParser()
+        investment_put_args.add_argument("id", type=str, required=True)
+        investment_put_args.add_argument("lycee", type=str, required=True)
+        investment_put_args.add_argument("ville", type=str, required=True)
+        investment_put_args.add_argument("year", type=int, required=True)
+
+        args = investment_put_args.parse_args()
+
+        data = fd.data(investments_path)
+        if args['id'] in list(data['codeuai']):
+            #data = data.to_dict()
+            return {'data': 'id already exists'}, 409 # conflict
+        else: 
+            data_ret = {
+                'codeuai': args['id'],
+                'lycee': args['lycee'],
+                'ville': args['ville'],
+                'annee_de_livraison': args['year']
+            }
+            return data_ret, 200
+    
+    @app.errorhandler(404)
+    def error_404(error):
+        message = "OOPS!! The requested URL was not found on the server.  Error 404!"
+        return make_response(jsonify({'error': message}), 404)
+
 # for getting investments based on cities
 class Investments_ville(Resource):
     def get(self, ville):
@@ -52,7 +81,7 @@ class Investments_ville(Resource):
         data = data.to_dict() 
         return data, 200
 
-# for getting investment/s based on Id
+# for getting an investment based on Id
 class Investments_id(Resource):
     # TO DO: add try error
     def get(self, id):
@@ -61,18 +90,14 @@ class Investments_id(Resource):
         data = data.to_dict() 
         return data, 200
 
-        
-def post(self):
-        #return {'data': 'test'}
-
-        args = investment_put_args.parse_args()  # parse arguments to dictionary
+    '''args = investment_put_args.parse_args()  # parse arguments to dictionary
 
         # read our CSV
         data = pd.read_csv('users.csv')
 
         return {'data': 'test'}
             
-        '''if args['company'] in list(data['entreprise']):
+        if args['company'] in list(data['entreprise']):
             return {
                 'message': f"'{args['userId']}' already exists."
             }, 401
