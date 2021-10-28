b32d0116cfddcb0e4d5f6e1cae4fe14d40720c77
architjen <architjen010@gmail.com>
2021-10-28 22:57:03 +0200

    adding path to config and returning errors

--- resources/fetch_data.py
+++ resources/fetch_data.py
@@ -2,6 +2,7 @@
 # (create an SQLAlchemy db, once all the test pass) USE: try-except-finally
 
 import pandas as pd
+import sys
 
 def data(investments_path):
     col_list = ['lycee', 'ville', 'annee_de_livraison', 'codeuai']
@@ -10,12 +11,17 @@ def data(investments_path):
         data_inv = data_inv[:5]
         return data_inv
     except FileNotFoundError as e:
-        print(e)
-        return None
+        #print(e)
+        return {'error': str(e)}
     except Exception as e:
-        print(e)
-        return None
+        #print(e)
+        return {'error': str(e)}
 
 if __name__ == "__main__":
-    investments_path = './data/investments.csv'
+    
+    # TO DO: --> make a package __init__.py
+    # adding config path to the sys.path, NOT A PYTHONIC WAY TO DO IT!!
+    sys.path.append( '/Users/aj/docs/scripts/git/python_rest_api')
+    import config
+    investments_path = config.PATH 
     d = data(investments_path) 
